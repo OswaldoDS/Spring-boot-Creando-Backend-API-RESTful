3293953636679408307ab2cca208a62b645c121b
Oswaldo <santoswaldos@gmail.com>
2026-04-29 21:26:49 -0600

    first commit

---
+++ src/main/java/com/oswaldo/curso/java/springboot/backend/Application.java
@@ -0,0 +1,13 @@
+package com.oswaldo.curso.java.springboot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/oswaldo/curso/java/springboot/backend/entities/Product.java
@@ -0,0 +1,9 @@
+package com.oswaldo.curso.java.springboot.backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Product {
+}

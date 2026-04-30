e607e3c9bfa09e784a9e7c1013ec6dec3455dc6d
Oswaldo <santoswaldos@gmail.com>
2026-04-29 21:55:23 -0600

    Implementando la interface y clase para sevice

---
+++ src/main/java/com/oswaldo/curso/java/springboot/backend/services/ProductService.java
@@ -0,0 +1,17 @@
+package com.oswaldo.curso.java.springboot.backend.services;
+
+import com.oswaldo.curso.java.springboot.backend.entities.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+    List<Product> findAll();
+
+    Optional<Product> findById(Long id);
+
+    Product save(Product product);
+
+    Optional<Product> deleteById(Long id);
+
+}
---
+++ src/main/java/com/oswaldo/curso/java/springboot/backend/services/ProductServiceImpl.java
@@ -0,0 +1,48 @@
+package com.oswaldo.curso.java.springboot.backend.services;
+
+import com.oswaldo.curso.java.springboot.backend.entities.Product;
+import com.oswaldo.curso.java.springboot.backend.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+    final private ProductRepository respository;
+
+    public ProductServiceImpl(ProductRepository respository) {
+        this.respository = respository;
+    }
+
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Product> findAll() {
+        return (List<Product>) respository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Product> findById(Long id) {
+        return respository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Product save(Product product) {
+        return respository.save(product);
+    }
+
+    @Override
+    @Transactional
+    public Optional<Product> deleteById(Long id) {
+        Optional<Product> productOptional = respository.findById(id);
+        if(productOptional.isPresent()){
+            respository.deleteById(id);
+            return productOptional;
+        }
+        return Optional.empty();
+    }
+}

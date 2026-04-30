fc5c19e7b8771aabc33b0d1d7a06103fbe4febbe
Oswaldo <santoswaldos@gmail.com>
2026-04-29 21:40:19 -0600

    Agregando etiquetas y métodos de la clase Product

--- src/main/java/com/oswaldo/curso/java/springboot/backend/entities/Product.java
+++ src/main/java/com/oswaldo/curso/java/springboot/backend/entities/Product.java
@@ -1,9 +1,47 @@
 package com.oswaldo.curso.java.springboot.backend.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Long precio;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Long precio) {
+        this.precio = precio;
+    }
+
 }
